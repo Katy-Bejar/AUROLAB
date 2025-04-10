f64c18b4f429cd73197839f25d0580d7b8d7d708
Katy-Bejar <kbejarr@unsa.edu.pe>
2025-04-10 18:06:01 -0500

    Corrigiendo issues

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["lh4.googleusercontent.com", "lh5.googleusercontent.com", "lh6.googleusercontent.com", "lh3.googleusercontent.com", "lh4.googleusercontent.com"],
+  },
+};
+
+export default nextConfig;
